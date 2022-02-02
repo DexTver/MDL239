97a87bd4faf9a9a964662bd0bf1c094b24c30870
dex_tver <ivan.sharapov.05@yandex.ru>
2022-02-02 18:09:50 +0300

    Update

--- Function/WeStudy.cs
+++ Function/WeStudy.cs
@@ -3,8 +3,43 @@ namespace Function;
 
 public static class WeStudy
 {
+    private static bool IsSort(string a)
+    {
+        string[] str = a.Split(' ');
+        for (int i = 1; i < str.Length; ++i)
+        {
+            if (str[i - 1].Length > str[i].Length)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static bool IsPolindrom(string a)
+    {
+        int n = a.Length;
+        for (int i = 0; i < n; ++i)
+        {
+            if (a[i] != a[n - i - 1])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static string Distance(string a, string firstA, string secondB)
+    {
+        int start = a.IndexOf(firstA) + firstA.Length;
+        int finish = a.LastIndexOf(secondB);
+        return a.Substring(start, finish - start);
+    }
     public static void Main(string[] args)
     {
-        
+        string x = Console.ReadLine();
+        //Console.WriteLine(IsSort(x));
+        //Console.WriteLine(IsPolindrom(x));
+        Console.WriteLine(Distance("сегодня урок будет в дистанционном формате", "сегодня", "будет"));
     }
 }
